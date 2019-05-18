8a4050e60d1726c907e2d6c88d1edf954d991134
Michał Kroszka <michal@myrpg.pl>
2019-05-18 21:09:27 +0200

    Add MyHexagonGrid class.

--- js/myHexagonGrid.js
+++ js/myHexagonGrid.js
@@ -1,1 +1,17 @@
 'use strict';
+
+class MyHexagonGrid {
+  constructor(x, y, container) {
+    this.x = x;
+    this.y = y;
+    this.hexagons = [];
+
+    this.GenerateHexagons();
+  }
+
+  GenerateHexagons() {
+    for (let rows = 0; this.y > rows; rows++)
+      for (let cols = 0; this.x > cols; cols++)
+        this.hexagons.push(new MyHexagon(cols, rows));
+  }
+}
